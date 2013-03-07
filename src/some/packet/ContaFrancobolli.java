86188ea061c2ede3be77d5c54193429367456953
Zanza00 <zanza00@gmail.com>
2013-03-07 19:03:03 +0100

    Creato metodo per elencare i francobolli
    Ora stampa quanti e quali francobolli servono per arrivare a quella
    cifra.

--- src/some/packet/ContaFrancobolli.java
+++ src/some/packet/ContaFrancobolli.java
@@ -3,26 +3,30 @@ package some.packet;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class ContaFrancobolli {
 
 	private static int[] valoreFrancobolli;
-	
+	private static int[] qualiFrancobolli;
+
 	/**
-	 * @return the availableCoins
+	 * @return valoreFrancobolli
 	 */
 	public int[] getValoreFrancobolli() {
 		return valoreFrancobolli;
 	}
 
 	/**
-	 * @param availableCoins the availableCoins to set
+	 * @param availableCoins
+	 *            the availableCoins to set
 	 */
 	public static void setValoreFrancobolli(int[] ListaFrancobolli) {
 		valoreFrancobolli = ListaFrancobolli;
+		qualiFrancobolli = new int[valoreFrancobolli.length];
 	}
+
 	public static void setValoreFrancobolli() {
-		valoreFrancobolli = new int[] {330, 300, 200, 140, 85, 75, 50,10, 5};
+		valoreFrancobolli = new int[] { 330, 300, 200, 140, 85, 75, 50, 10, 5 };
+		qualiFrancobolli = new int[valoreFrancobolli.length];
 	}
 
 	private int amount = 0;
@@ -36,27 +40,30 @@ public class ContaFrancobolli {
 		return makeChange(amount, 0, current);
 	}
 
-	public List<Integer> makeChange(int amount, int currentStamp, List<Integer> current) {
+	public List<Integer> makeChange(int amount, int currentStamp,
+			List<Integer> current) {
 
-		
-		if (amount == 0) { 			// if amount = zero, we are at the bottom of a successful recursion
+		if (amount == 0) { // if amount = zero, we are at the bottom of a
+							// successful recursion
 			// return current solution
 			return current;
-		} else if (amount < 0) {	// check to see if we went too far
+		} else if (amount < 0) { // check to see if we went too far
 			// reject current solution, we went too far
 			return null;
-		} else if (currentStamp >= valoreFrancobolli.length) {		// no more coin to check
+		} else if (currentStamp >= valoreFrancobolli.length) { // no more coin
+																// to check
 			// reject current solution, no more coin to check
 			return null;
 		} else {
 			List<Integer> pathA = new ArrayList<Integer>(current);
-			// go on skipping the currentCoin 
+			// go on skipping the currentStamp
 			pathA = makeChange(amount, currentStamp + 1, pathA);
-			
+
 			List<Integer> pathB = new ArrayList<Integer>(current);
-			// go on adding the currentCoin to the solution 
+			// go on adding the currentStamp to the solution
 			pathB.add(Integer.valueOf(valoreFrancobolli[currentStamp]));
-			pathB = makeChange(amount - valoreFrancobolli[currentStamp], currentStamp, pathB);
+			pathB = makeChange(amount - valoreFrancobolli[currentStamp],
+					currentStamp, pathB);
 
 			if (pathA == null && pathB == null) {
 				return null;
@@ -67,35 +74,55 @@ public class ContaFrancobolli {
 			if (pathB == null) {
 				return pathA;
 			}
-			
+
 			return pathA.size() < pathB.size() ? pathA : pathB;
 		}
 	}
 
-	public static void main(String[] args) {
-		System.out.println("inserire la cifra da dividere");
-		double importoIniziale = TextIO.getDouble();
-		
-		int amount = (int) (importoIniziale *100);
-		
-		setValoreFrancobolli();
-		
-		ContaFrancobolli f = new ContaFrancobolli(amount);
-		List<Integer> change = f.makeChange();
-		
-		System.out.println("la cifra inserita è: " + importoIniziale + "€");
-		System.out.println("di seguito sono elencati i tagli necessari");
-		
-		if (change == null) {
-			System.out.println("Nessuna soluzione, controlla la cifra inserita");
-		} else {
-			for (int i = 0; i < change.size(); i++) {
-				System.out.print((double) change.get(i)/100 );
-				if (i != change.size() - 1 ) {
-					System.out.print(" + ");
+	/**
+	 * 
+	 * @param change
+	 *            una lista di valori di francobolli
+	 */
+	public static void quantiFrancobolli(List<Integer> change) {
+		if (change != null) {
+			int i = 0, j = 0;
+			while (j < change.size()) {
+				if (change.get(j) == valoreFrancobolli[i]) {
+					qualiFrancobolli[i]++;
+					j++;
+				} else {
+					i++;
 				}
 			}
-			
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Inseririsci l'importo da suddividere");
+		double importoIniziale = TextIO.getDouble();
+
+		int amount = (int) (importoIniziale * 100);
+
+		setValoreFrancobolli();
+
+		ContaFrancobolli f = new ContaFrancobolli(amount);
+		List<Integer> change = f.makeChange();
+
+
+		System.out.println("la cifra inserita è: " + importoIniziale + "€");
+
+
+		if (change == null) {
+			System.out.println("Nessuna soluzione trovata, controlla che la cifra inserita sia corretta");
+		} else {
+			quantiFrancobolli(change);			
+			System.out.println("di seguito sono elencati i tagli necessari");
+			for (int i = 0; i < qualiFrancobolli.length; i++) {
+				if (qualiFrancobolli[i] > 0) {
+					System.out.println("Numero francobolli da "	+ (double) valoreFrancobolli[i]/100 + " €: " + qualiFrancobolli[i]);
+				}
+			}
 		}
 	}
 }
