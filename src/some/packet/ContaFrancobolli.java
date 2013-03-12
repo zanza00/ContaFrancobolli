b9771e053f726b86bf5c33101b5b3d750350e65a
Zanza00 <zanza00@gmail.com>
2013-03-12 18:01:13 +0100

    Metodo per rimuovere un francobollo dalla lista

--- src/some/packet/ContaFrancobolli.java
+++ src/some/packet/ContaFrancobolli.java
@@ -3,6 +3,7 @@ package some.packet;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class ContaFrancobolli {
 
@@ -10,16 +11,16 @@ public class ContaFrancobolli {
 	private static int[] qualiFrancobolli; //array per il numero di francobolli di quel taglio necessari per arrivare all'importo
 	private static int[] valoreFrancobolli; // il valore nominale dei francobolli disponibili
 	private int importo; //l'importo richiesto 
-	
+
 	/*
 	 * costruttore della classe
 	 */
 	public ContaFrancobolli(int amount) {
 		this.importo = amount;
 	}
-	
+
 	/*
-	 * Di seguito i metodi per gestire l'array valoreFrancobolli 
+	 * Di seguito i metodi per gestire l'array valoreFrancobolli
 	 */
 	/**
 	 * @return valoreFrancobolli
@@ -27,7 +28,7 @@ public class ContaFrancobolli {
 	public int[] getValoreFrancobolli() {
 		return valoreFrancobolli;
 	}
-	
+
 	/**
 	 * @param availableCoins
 	 *            the availableCoins to set
@@ -36,54 +37,72 @@ public class ContaFrancobolli {
 		valoreFrancobolli = ListaFrancobolli;
 		qualiFrancobolli = new int[valoreFrancobolli.length];
 	}
-	
+
 	public void setValoreFrancobolli() {
 		int[] listaFb = { 330, 300, 200, 140, 85, 75, 50, 10, 5 };
 		setValoreFrancobolli(listaFb);
 	}
+
 	/**
 	 * 
-	 * @param d il nuovo valore del francobollo da aggiungere all'array dei valori 
-	 * <p>può essere sia int che double, se è int deve essere già moltiplicato per 100
-	 * <p> ad esempio 3.42 deve essere inserito come 342
+	 * @param d
+	 *            il nuovo valore del francobollo da aggiungere all'array dei
+	 *            valori
+	 *            <p>
+	 *            può essere sia int che double, se è int deve essere già
+	 *            moltiplicato per 100
+	 *            <p>
+	 *            ad esempio 3.42 deve essere inserito come 342
 	 */
 	public void addNuovoFrancobollo(int valore) {
-		int [] tempArray = new int[valoreFrancobolli.length+1];
-		// copio i valori dall'array orginale a quello temporaneo 
-		System.arraycopy(valoreFrancobolli, 0, tempArray, 0, valoreFrancobolli.length);
-		//aggiungo i valori all'array temporaneo
-		tempArray[valoreFrancobolli.length]= valore;
-		// faccio puntare valoreFrancobolli all'array temporaneo
-		valoreFrancobolli = tempArray;
-		//riordino l'array per non rompere l'algoritmo 
+		// aggiungo il valore alla fine dell'array
+		valoreFrancobolli = ArrayUtils.add(valoreFrancobolli, valore);
+		// riordino l'array per non rompere l'algoritmo
 		ordinaValoreFrancobolli();
 	}
-	
-	public void addNuovoFrancobollo(double d){
-		int val = (int) (d*100);
+
+	public void addNuovoFrancobollo(double d) {
+		int val = (int) (d * 100);
 		addNuovoFrancobollo(val);
-		
 	}
-	
+
 	/**
-	 * riordina in ordine decrescente i valori dei francobolli nell'array valoriFrancobolli;
+	 * riordina in ordine decrescente i valori dei francobolli nell'array
+	 * valoriFrancobolli;
 	 */
-	private static void ordinaValoreFrancobolli(){
-		//uso sort() per riordinare in ordine ascendente
+	private static void ordinaValoreFrancobolli() {
+		// uso sort() per riordinare in ordine ascendente
 		Arrays.sort(valoreFrancobolli);
-		//metto l'array in ordine decrescente
-		for (int sinistra=0, destra=valoreFrancobolli.length-1; sinistra<destra; sinistra++, destra--) {
-		    int temp = valoreFrancobolli[sinistra];
-		    valoreFrancobolli[sinistra]  = valoreFrancobolli[destra];
-		    valoreFrancobolli[destra] = temp;
-		}
+		// metto l'array in ordine decrescente
+		ArrayUtils.reverse(valoreFrancobolli);
 	}
-	
-	public int getQualiFrancobolliLength(){
+
+	/**
+	 * 
+	 * @param valore
+	 *            il valore del francobollo da eliminare moltiplicato per 100
+	 *            <p>
+	 *            ad esempio 3.30€ diventa 330
+	 * 
+	 */
+	public void removeUnFrancobollo(int valore) {
+		valoreFrancobolli = ArrayUtils.removeElement(valoreFrancobolli, valore);
+	}
+
+	/**
+	 * 
+	 * @param valore
+	 *            il valore del francobollo da eliminare
+	 */
+	public void removeUnFrancobollo(double valore) {
+		int tmp = (int) (valore * 100);
+		removeUnFrancobollo(tmp);
+	}
+
+	public int getQualiFrancobolliLength() {
 		return qualiFrancobolli.length;
 	}
-	
-	
+
 	/*
 	 * L'algoritmo che trova il minor numero di francobolli necessari
 	 */
@@ -93,8 +112,10 @@ public class ContaFrancobolli {
 	public static List<Integer> getChange() {
 		return change;
 	}
+
 	/**
-	 * @param change the change to set
+	 * @param change
+	 *            the change to set
 	 */
 	public void setChange(List<Integer> change) {
 		ContaFrancobolli.change = change;
@@ -108,15 +129,16 @@ public class ContaFrancobolli {
 	public List<Integer> makeChange(int amount, int currentStamp,
 			List<Integer> current) {
 
-		if (amount == 0) {  // if amount = zero, we are at the bottom of a
+		if (amount == 0) { // if amount = zero, we are at the bottom of a
 							// successful recursion
-			                // return current solution
+							// return current solution
 			return current;
 		} else if (amount < 0) { // check to see if we went too far
- 			// reject current solution, we went too far
-			return null; 
+			// reject current solution, we went too far
+			return null;
 
-		} else if (currentStamp >= valoreFrancobolli.length) { // no more coin to check
+		} else if (currentStamp >= valoreFrancobolli.length) { // no more coin
+																// to check
 			// reject current solution, no more coin to check
 			return null;
 		} else {
@@ -143,13 +165,14 @@ public class ContaFrancobolli {
 			return pathA.size() < pathB.size() ? pathA : pathB;
 		}
 	}
-	
+
 	/*
 	 * 
 	 */
 	/**
 	 * 
-	 * @param i la posizione nell'array
+	 * @param i
+	 *            la posizione nell'array
 	 * @return il valore per quell'indice
 	 */
 	public int qualeFrancobollo(int i) {
@@ -175,7 +198,7 @@ public class ContaFrancobolli {
 		}
 	}
 
-	public int valoreFrancobollo(int i){
+	public int valoreFrancobollo(int i) {
 		return valoreFrancobolli[i];
 	}
 }
--- src/some/packet/Test.java
+++ src/some/packet/Test.java
@@ -14,6 +14,8 @@ public class Test {
 		contaFb.setValoreFrancobolli();
 		contaFb.addNuovoFrancobollo(3.50);
 		contaFb.addNuovoFrancobollo(342);
+		contaFb.removeUnFrancobollo(140);
+		contaFb.removeUnFrancobollo(0.75);
 		int[] test = contaFb.getValoreFrancobolli();
 		System.out.println(Arrays.toString(test));;
 	}
